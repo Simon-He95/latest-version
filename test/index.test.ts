7655351c43723ef5f50ed21ca16a06ec34db7efd
Nelly Dylan <308813800@qq.com>
2024-09-20 23:58:17 +0800

    fix(): 改成异步并且新增测试

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,20 @@
 import { describe, expect, it } from 'vitest'
+import { latestVersion } from '../src'
 
 describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+  it('npmpkg', async () => {
+    const version = await latestVersion('vue', { version: '2.7.16' })
+    expect(version).toBe('2.7.16')
+  })
+
+  it('tag', async () => {
+    const version = await latestVersion('vue', { version: 'legacy' })
+    expect(version).toBe('2.7.16')
+  })
+
+  it('cwd', async () => {
+    // 测试使用用户目录的.npmrc里面配置的源来找包
+    const version = await latestVersion('abort-controller', { version: 'latest', cwd: process.cwd() })
+    expect(version).toBe('3.0.0')
   })
 })
