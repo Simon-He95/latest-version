f3bfcde6c3684b2f06318dedf3f3f55658e1b78a
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-11-16 22:54:57 +0800

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,37 @@ import { fetchLatestVersion } from './fetchLatestVersion'
 export async function latestVersion(pkgname: string, options: { version?: string, concurrency?: number } = {}) {
   const { version = 'latest', concurrency = 1 } = options
   return cancellablePromiseAny([
-    ...Array.from({ length: concurrency }, () => fetchVersion(pkgname, version)),
+    ...Array.from({ length: concurrency }, () => fetchVersionWithView(pkgname, version)),
+    ...Array.from({ length: concurrency }, () => fetchVersionWithShow(pkgname, version)),
     ...Array.from({ length: concurrency }, () => fetchLatestVersion(pkgname, version)),
   ])
 }
 
-function fetchVersion(pkgname: string, version: string) {
+function fetchVersionWithShow(pkgname: string, version: string) {
   return new Promise<string>((resolve, reject) => {
     const { result, status } = jsShell(`npm show ${pkgname} --json`, 'pipe')
+    if (status !== 0) {
+      reject(result)
+      return
+    }
+    try {
+      const { versions, 'dist-tags': distTags } = JSON.parse(result)
+      if (distTags[version]) {
+        resolve(distTags[version])
+        return
+      }
+      const ver = (versions as any).findLast((item: any) => item.startsWith(version))
+      resolve(ver || versions[versions.length - 1])
+    }
+    catch (error) {
+      reject(error)
+    }
+  })
+}
+
+function fetchVersionWithView(pkgname: string, version: string) {
+  return new Promise<string>((resolve, reject) => {
+    const { result, status } = jsShell(`npm view ${pkgname} --json`, 'pipe')
     if (status !== 0) {
       reject(result)
       return
