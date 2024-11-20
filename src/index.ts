1e42693a5fdefaa5721256e84393a45c32982b65
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-11-20 15:25:20 +0800

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import { fetchLatestVersion } from './fetchLatestVersion'
 
 export async function latestVersion(pkgname: string, options: { version?: string, concurrency?: number } = {}) {
   const { version = 'latest', concurrency = 1 } = options
-  return Promise.any([
-    ...Array.from({ length: concurrency }, () => fetchVersionWithView(pkgname, version)),
-    ...Array.from({ length: concurrency }, () => fetchVersionWithShow(pkgname, version)),
-    ...Array.from({ length: concurrency }, () => fetchLatestVersion(pkgname, version)),
-  ])
+  return retryAsync(async () => Promise.any([
+    fetchVersionWithView(pkgname, version),
+    fetchVersionWithShow(pkgname, version),
+    fetchLatestVersion(pkgname, version),
+  ]), concurrency)
 }
 
 function fetchVersionWithShow(pkgname: string, version: string) {
@@ -52,4 +52,18 @@ function fetchVersionWithView(pkgname: string, version: string) {
       reject(error)
     }
   })
+}
+
+async function retryAsync<T>(fn: () => Promise<T>, retries: number): Promise<T> {
+  try {
+    return await fn()
+  }
+  catch (error: any) {
+    if (retries > 0) {
+      return retryAsync(fn, retries - 1)
+    }
+    else {
+      throw error
+    }
+  }
 }
